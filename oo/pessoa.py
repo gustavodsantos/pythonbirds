8b3d739e04032395a4040ff7b8b363e3a9d4e39f
gustavodsantos <gustavojuniordos@hotmail.com>
2023-12-31 14:25:50 -0300

    Criados atributos de instãncia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=37):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Greyce')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Gustavo'
+    print(p.nome)
+
